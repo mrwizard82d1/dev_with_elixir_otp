5eedc62bc229be479d72ada4f63179552386d75a
Larry Jones <mrwizard82d1@gmail.com>
2025-06-08 04:24:29 -0500

    Refactor to be more idiomatic Elixir

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -7,8 +7,12 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    first_line = request |> String.split("\n") |> List.first
-    [method, path, _] = String.split(first_line, " ")
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+
     %{method: method, path: path, resp_body: ""}
   end
 
