bde4c883ddbcf1e74ce0d1648627ab5e077d45f4
Larry Jones <mrwizard82d1@gmail.com>
2025-06-09 13:50:46 -0500

    BROKE Added request for "/big_foot"
    We'll fix this error in the next video.

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -102,3 +102,15 @@ Accept */*
 response = Servy.Handler.handle(request)
 
 IO.puts(response)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
