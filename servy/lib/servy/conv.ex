ba9120c8a2b8641a36107f760e0f19abff24f96c
Larry Jones <mrwizard82d1@gmail.com>
2025-06-12 21:46:32 -0500

    Move functions related to Struct.Conv to module

--- servy/lib/servy/conv.ex
+++ servy/lib/servy/conv.ex
@@ -1,3 +1,18 @@
 defmodule Servy.Conv do
   defstruct method: "", path: "", resp_body: "", status_code: nil
+
+  def full_status(conv) do
+    "#{conv.status_code} #{status_code_to_reason_phrase(conv.status_code)}"
+  end
+
+  defp status_code_to_reason_phrase(status_code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not found",
+      500 => "Internal Server Error",
+    }[status_code]
+  end
 end
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -119,23 +119,12 @@ defmodule Servy.Handler do
     # interpolation to "splice" in fields.
     #
     """
-    HTTP/1.1 #{conv.status_code} #{status_code_to_reason_phrase(conv.status_code)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_code_to_reason_phrase(status_code) do
-    %{
-    200 => "OK",
-    201 => "Created",
-    401 => "Unauthorized",
-    403 => "Forbidden",
-    404 => "Not found",
-    500 => "Internal Server Error",
-    }[status_code]
   end
 end
 
