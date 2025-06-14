07893e07c9c119247a4ac2d827d0438ba59a9b0e
Larry Jones <mrwizard82d1@gmail.com>
2025-06-14 18:54:55 -0500

    Parse request fully
    - Request line
    - Header line(s)
    - Blank line
    - Body

--- servy/lib/servy/conv.ex
+++ servy/lib/servy/conv.ex
@@ -1,5 +1,9 @@
 defmodule Servy.Conv do
-  defstruct method: "", path: "", resp_body: "", status_code: nil
+  defstruct method: "",
+            path: "",
+            params: %{},
+            resp_body: "",
+            status_code: nil
 
   def full_status(conv) do
     "#{conv.status_code} #{status_code_to_reason_phrase(conv.status_code)}"
--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -4,19 +4,28 @@ defmodule Servy.Parser do
 
   def parse(request) do
     # Split on the blank line (two newline characters) before the request body.
-    [top, _params_string] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\n\n")
 
     # Split the "top" into the request line and the header **lines**.
     [request_line | _header_lines] = String.split(top, "\n")
 
     [method, path, _] = String.split(request_line, " ")
 
+    params = parse_params(params_string)
+
     # This implementation works, but it is insufficient. (It does not capture
     # the HTTP headers.)
 
     %Conv{
       method: method,
-      path: path
+      path: path,
+      params: params,
     }
+  end
+
+  defp parse_params(params_string) do
+    params_string
+    |> String.trim
+    |> URI.decode_query
   end
 end
