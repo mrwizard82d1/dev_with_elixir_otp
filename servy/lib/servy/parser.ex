b8bbbb7001c27ce5a0a4a8bee537a4e5b1503545
Larry Jones <mrwizard82d1@gmail.com>
2025-06-14 18:45:15 -0500

    Split POST into its components
    - Request line
    - Header line(s)
    - Body

--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -3,11 +3,16 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
+    # Split on the blank line (two newline characters) before the request body.
+    [top, _params_string] = String.split(request, "\n\n")
+
+    # Split the "top" into the request line and the header **lines**.
+    [request_line | _header_lines] = String.split(top, "\n")
+
+    [method, path, _] = String.split(request_line, " ")
+
+    # This implementation works, but it is insufficient. (It does not capture
+    # the HTTP headers.)
 
     %Conv{
       method: method,
