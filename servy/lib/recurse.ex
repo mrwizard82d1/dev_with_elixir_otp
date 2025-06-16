040ef49dfa954b920e7079d5d5684d7acb15964a
Larry Jones <mrwizard82d1@gmail.com>
2025-06-16 14:44:28 -0500

    Add triple function
    Again, my implementation **is not** tail recursive. And I realize it is
    my mistake (not reading the instructions carefully enough.)

--- servy/lib/recurse.ex
+++ servy/lib/recurse.ex
@@ -5,7 +5,19 @@ defmodule Recurse do
   end
 
   def sum([]), do: 0
+
+  def triple([head | tail]) do
+    IO.puts "Head: #{head}, Tail: #{inspect(tail)}"
+    [3 * head | triple(tail)]
+  end
+
+  def triple([]), do: []
 end
 
 total = Recurse.sum([1, 2, 3, 4, 5])
 IO.puts "The total is #{total}"
+
+IO.puts ""
+
+tripled = Recurse.triple([1, 2, 3, 4, 5])
+IO.puts "The tripled list is #{inspect(tripled)}"
