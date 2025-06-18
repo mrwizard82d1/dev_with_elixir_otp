174c7eb0c382c883f38e14f19f63f5c5f79b43b5
Larry Jones <mrwizard82d1@gmail.com>
2025-06-17 21:11:28 -0500

    Using recursion more

--- servy/lib/recurse.ex
+++ servy/lib/recurse.ex
@@ -14,6 +14,12 @@ defmodule Recurse do
   defp triple([], result) do
     Enum.reverse(result)
   end
+
+  def my_map([h | t], f) do
+    [f.(h) | my_map(t, f)]
+  end
+
+  def my_map([], _f), do: []
 end
 
 total = Recurse.sum([1, 2, 3, 4, 5], 0)
@@ -23,3 +29,14 @@ IO.puts ""
 
 tripled = Recurse.triple([1, 2, 3, 4, 5])
 IO.puts "The tripled list is #{inspect(tripled)}"
+
+double = &(2 * &1)
+triple = &(3 * &1)
+quadruple = &(4 * &1)
+
+doubled = Recurse.my_map([1, 2, 3, 4, 5], double)
+IO.puts "The mapped list using double/1 is #{inspect(doubled)}"
+tripled = Recurse.my_map([1, 2, 3, 4, 5], triple)
+IO.puts "The mapped list using triple/1 is #{inspect(tripled)}"
+quadrupled = Recurse.my_map([1, 2, 3, 4, 5], quadruple)
+IO.puts "The mapped list using quadruple/1 is #{inspect(quadrupled)}"
