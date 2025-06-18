72708a249d91d33d7ec91b75fbb4c678be365824
Larry Jones <mrwizard82d1@gmail.com>
2025-06-18 14:50:27 -0500

    Change controller and template to consume content
    Previous commits embedded HTML tags in the output displayed to the user.
    
    This commit changes the implementation to put these tags in a template
    and then to have the implementation use that template to inspect that
    display data.

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -3,21 +3,21 @@ defmodule Servy.BearController do
   alias Servy.Bear
   alias Servy.WildThings
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
-    items =
+    bears =
       WildThings.list_bears()
-      |> Enum.filter(&Bear.grizzly?/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join
+
+    content =
+      @templates_path
+      |> Path.join("index.eex")
+      |> EEx.eval_file(bears: bears)
 
     %{
       conv |
-      resp_body: "<ul>#{items}</ul>" ,
+      resp_body: content ,
       status_code: 200,
     }
   end
