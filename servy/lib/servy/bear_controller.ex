e02ee9f4ecd83586ce9a04c910d388ab3b0e7f09
Larry Jones <mrwizard82d1@gmail.com>
2025-06-18 16:45:49 -0500

    Extract common code into common function, render/3

--- servy/lib/servy/bear_controller.ex
+++ servy/lib/servy/bear_controller.ex
@@ -5,15 +5,11 @@ defmodule Servy.BearController do
 
   @templates_path Path.expand("../../templates", __DIR__)
 
-  def index(conv) do
-    bears =
-      WildThings.list_bears()
-      |> Enum.sort(&Bear.order_asc_by_name/2)
-
+  def render(conv, template_file_name, bindings \\ []) do
     content =
       @templates_path
-      |> Path.join("index.eex")
-      |> EEx.eval_file(bears: bears)
+      |> Path.join(template_file_name)
+      |> EEx.eval_file(bindings)
 
     %{
       conv |
@@ -22,22 +18,18 @@ defmodule Servy.BearController do
     }
   end
 
+  def index(conv) do
+    bears =
+      WildThings.list_bears()
+      |> Enum.sort(&Bear.order_asc_by_name/2)
+
+    render(conv, "index.eex", bears: bears)
+  end
+
   def show(conv, %{"id" => id}) do
     bear = WildThings.get_bear(id)
 
-    IO.puts "Showing the bear with id=#{id}"
-    IO.inspect bear
-
-    content =
-      @templates_path
-      |> Path.join("show.eex")
-      |> EEx.eval_file(bear: bear)
-
-    %{
-      conv |
-      resp_body: content,
-      status_code: 200,
-    }
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
