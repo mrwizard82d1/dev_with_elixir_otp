2174b239ebec537e6c1b05e902a0c09cc73e57a7
Larry Jones <mrwizard82d1@gmail.com>
2025-06-19 17:13:04 -0500

    Replace manual "pages" with `resources` generation

--- refuge/lib/refuge_web/router.ex
+++ refuge/lib/refuge_web/router.ex
@@ -17,18 +17,21 @@ defmodule RefugeWeb.Router do
   scope "/", RefugeWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
-    # The video uses the value, `:index` unchanged from the generated code
-    # Since my code generates `:home`, I assume I use the same "page"
-    get "/bears", BearController, :home
-    # Here are the additional routes from the video
-    get "/bears/new", BearController, :new
-    get "/bears/:id", BearController, :show
-    get "/bears/:id/edit", BearController, :edit
-    post "/bears", BearController, :create
-    put "/bears:id", BearController, :update
-    patch "/bears:id", BearController, :update
-    delete "/bears:id", BearController, :delete
+    # get "/", PageController, :home
+    # # The video uses the value, `:index` unchanged from the generated code
+    # # Since my code generates `:home`, I assume I use the same "page"
+    # get "/bears", BearController, :home
+    # # Here are the additional routes from the video
+    # get "/bears/new", BearController, :new
+    # get "/bears/:id", BearController, :show
+    # get "/bears/:id/edit", BearController, :edit
+    # post "/bears", BearController, :create
+    # put "/bears:id", BearController, :update
+    # patch "/bears:id", BearController, :update
+    # delete "/bears:id", BearController, :delete
+
+    # Here is the shortcut to add **all** routes at once
+    resources "/bears", BearController
   end
 
   # Other scopes may use custom stacks.
