2fe63c1e7ab6bbc930879e2bcc739cc108de634a
Larry Jones <mrwizard82d1@gmail.com>
2025-06-20 13:42:11 -0500

    Passing Servy.Parser doctests

--- servy/lib/servy/parser.ex
+++ servy/lib/servy/parser.ex
@@ -1,5 +1,4 @@
 defmodule Servy.Parser do
-
   alias Servy.Conv
 
   def parse(request) do
@@ -20,23 +19,33 @@ defmodule Servy.Parser do
       method: method,
       path: path,
       params: params,
-      headers: headers,
+      headers: headers
     }
   end
 
   def parse_headers(header_lines) do
-    Enum.reduce(header_lines, %{},
-      fn x, acc ->
-        [key, value] = String.split(x, ": ")
-        Map.put(acc, key, value)
+    Enum.reduce(header_lines, %{}, fn x, acc ->
+      [key, value] = String.split(x, ": ")
+      Map.put(acc, key, value)
     end)
   end
 
-  defp parse_params("application/x-www-form-urlencoded", params_string) do
+  @doc """
+  Parses the given param string oft he form `key1=value1&key2=value2`
+  into a map with corresponding keys and values.
+
+  ## Examples
+    iex> params_string = "name=Baloo&type=Brown"
+    iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", params_string)
+    %{"name" => "Baloo", "type" => "Brown"}
+    iex> Servy.Parser.parse_params("multipart/form_data", params_string)
+    %{}
+  """
+  def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string
-    |> String.trim
-    |> URI.decode_query
+    |> String.trim()
+    |> URI.decode_query()
   end
 
-  defp parse_params(_, _), do: %{}
+  def parse_params(_, _), do: %{}
 end
