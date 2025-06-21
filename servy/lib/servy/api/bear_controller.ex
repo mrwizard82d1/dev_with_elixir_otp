fb79c3efaf284dd9617b5d54530cb734aad5b6f6
Larry Jones <mrwizard82d1@gmail.com>
2025-06-21 16:27:03 -0500

    Put content length into response header

--- servy/lib/servy/api/bear_controller.ex
+++ servy/lib/servy/api/bear_controller.ex
@@ -4,11 +4,19 @@ defmodule Servy.Api.BearController do
       Servy.WildThings.list_bears()
       |> Poison.encode!()
 
+    conv = put_resp_content_type(conv, "application/json")
+
     %{
       conv
       | status_code: 200,
-        resp_headers: Map.put(conv.resp_headers, "Content-Type", "application/json"),
         resp_body: json
+    }
+  end
+
+  def put_resp_content_type(conv, content_type) do
+    %{
+      conv
+      | resp_headers: Map.put(conv.resp_headers, "Content-Type", content_type)
     }
   end
 end
