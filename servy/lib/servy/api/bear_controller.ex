7673c5ad37589d1fd25a04d313ae14e3e0b2d179
Larry Jones <mrwizard82d1@gmail.com>
2025-06-21 16:20:23 -0500

    Exercise: support multiple response headers

--- servy/lib/servy/api/bear_controller.ex
+++ servy/lib/servy/api/bear_controller.ex
@@ -4,6 +4,11 @@ defmodule Servy.Api.BearController do
       Servy.WildThings.list_bears()
       |> Poison.encode!()
 
-    %{conv | status_code: 200, resp_content_type: "application/json", resp_body: json}
+    %{
+      conv
+      | status_code: 200,
+        resp_headers: Map.put(conv.resp_headers, "Content-Type", "application/json"),
+        resp_body: json
+    }
   end
 end
--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -123,7 +123,7 @@ defmodule Servy.Handler do
     #
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: #{conv.resp_content_type}\r
+    Content-Type: #{conv.resp_headers["Content-Type"]}\r
     Content-Length: #{byte_size(conv.resp_body)}\r
     \r
     #{conv.resp_body}
