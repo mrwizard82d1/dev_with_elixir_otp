70f714f8bff912aabdc3a3c6afc3bb8d63084e66
Larry Jones <mrwizard82d1@gmail.com>
2025-06-25 18:01:14 -0500

    Unit test of our HttpServer function

--- servy/test/http_server_test.exs
+++ servy/test/http_server_test.exs
@@ -2,6 +2,7 @@ defmodule HttpServerTest do
   use ExUnit.Case
 
   alias Servy.HttpServer
+  alias Servy.HttpClient
 
   @moduletag :capture_log
 
@@ -9,5 +10,31 @@ defmodule HttpServerTest do
 
   test "module exists" do
     assert is_list(HttpServer.module_info())
+  end
+
+  test "accepts a request on a socket and sends back the respons" do
+    spawn(HttpServer, :start, [test_port()])
+
+    request = """
+    GET /wildthings HTTP/1.1\r
+    Host: example.com\r
+    User-Agent: ExampleBrowser/1.0\r
+    Accept: */*\r
+    \r
+    """
+
+    response = HttpClient.send_request(request)
+
+    assert response == """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 20\r
+    \r
+    Bears, Lions, Tigers
+    """
+  end
+
+  defp test_port do
+    4000
   end
 end
