4a2116aea4cf3c649c8393d5a9c9ea9bfecec142
Larry Jones <mrwizard82d1@gmail.com>
2025-06-27 02:30:08 -0500

    Execute 5 parallel GET requests

--- servy/test/http_server_test.exs
+++ servy/test/http_server_test.exs
@@ -15,11 +15,77 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back the respons" do
     spawn(HttpServer, :start, [test_port()])
 
-    {:ok, %{status_code: status_code, body: body}} =
-      HTTPoison.get("http://localhost:4000/wildthings")
+    parent = self()
 
-    assert status_code == 200
-    assert body == "Bears, Lions, Tigers"
+    spawn(fn ->
+      result = HTTPoison.get("http://localhost:4000/wildthings")
+      send(parent, result)
+    end)
+
+    spawn(fn ->
+      result = HTTPoison.get("http://localhost:4000/wildthings")
+      send(parent, result)
+    end)
+
+    spawn(fn ->
+      result = HTTPoison.get("http://localhost:4000/wildthings")
+      send(parent, result)
+    end)
+
+    spawn(fn ->
+      result = HTTPoison.get("http://localhost:4000/wildthings")
+      send(parent, result)
+    end)
+
+    spawn(fn ->
+      result = HTTPoison.get("http://localhost:4000/wildthings")
+      send(parent, result)
+    end)
+
+    receive do
+      {:ok, %{status_code: status_code, body: body}} ->
+        assert status_code == 200
+        assert body = "Bears, Lions, Tigers"
+
+      unexpected ->
+        assert false, "Unexpected #{unexpected}"
+    end
+
+    receive do
+      {:ok, %{status_code: status_code, body: body}} ->
+        assert status_code == 200
+        assert body = "Bears, Lions, Tigers"
+
+      unexpected ->
+        assert false, "Unexpected #{unexpected}"
+    end
+
+    receive do
+      {:ok, %{status_code: status_code, body: body}} ->
+        assert status_code == 200
+        assert body = "Bears, Lions, Tigers"
+
+      unexpected ->
+        assert false, "Unexpected #{unexpected}"
+    end
+
+    receive do
+      {:ok, %{status_code: status_code, body: body}} ->
+        assert status_code == 200
+        assert body = "Bears, Lions, Tigers"
+
+      unexpected ->
+        assert false, "Unexpected #{unexpected}"
+    end
+
+    receive do
+      {:ok, %{status_code: status_code, body: body}} ->
+        assert status_code == 200
+        assert body = "Bears, Lions, Tigers"
+
+      unexpected ->
+        assert false, "Unexpected #{unexpected}"
+    end
   end
 
   defp test_port do
