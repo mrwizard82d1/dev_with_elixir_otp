7200315f5a13c5adc75391236a7202745339393c
Larry Jones <mrwizard82d1@gmail.com>
2025-06-27 13:32:37 -0500

    "Works" but results in unexpected order
    Our code implicitly expected results to arrive in the order we spawned
    the `async/1` function; however, Servy.Tracker.get_location/1 only
    sleeps for 500 ms but each call to Servy.VideoCampg.get_snaphshot/1
    sleeps for 2000 ms.
    
    We need to change our code to distinguish between snapshots and
    locations. Hmm...
    
    As indicated further along in the video, we can actually "control the
    order" by using the PID of each spawned process.

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -26,19 +26,26 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{method: "GET", path: "/snapshots"} = conv) do
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
     # spawn(fn -> send(parent, {:result, Servy.VideoCam.get_snapshot(camera_name)}) end)
     Fetcher.async(fn -> Servy.VideoCam.get_snapshot("cam-1") end)
     Fetcher.async(fn -> Servy.VideoCam.get_snapshot("cam-2") end)
     Fetcher.async(fn -> Servy.VideoCam.get_snapshot("cam-3") end)
 
+    Fetcher.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
     snapshot1 = Fetcher.get_result()
     snapshot2 = Fetcher.get_result()
     snapshot3 = Fetcher.get_result()
+    where_is_bigfoot = Fetcher.get_result()
 
     snapshots = [snapshot1, snapshot2, snapshot3]
 
-    %{conv | status_code: 200, resp_body: inspect(snapshots)}
+    %{
+      conv
+      | status_code: 200,
+        resp_body: inspect({snapshots, where_is_bigfoot})
+    }
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"} = _conv) do
