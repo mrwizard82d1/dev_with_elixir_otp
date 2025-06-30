d0fdb75583b591f3440f3fab581f5d953033786c
Larry Jones <mrwizard82d1@gmail.com>
2025-06-30 13:54:20 -0500

    Comment out module testing code

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -47,14 +47,14 @@ defmodule Servy.PledgeServer do
   end
 end
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-PledgeServer.start()
+# PledgeServer.start()
 
-IO.inspect(PledgeServer.create_pledge("larry", 10))
-IO.inspect(PledgeServer.create_pledge("moe", 20))
-IO.inspect(PledgeServer.create_pledge("curly", 30))
-IO.inspect(PledgeServer.create_pledge("daisy", 40))
-IO.inspect(PledgeServer.create_pledge("grace", 50))
+# IO.inspect(PledgeServer.create_pledge("larry", 10))
+# IO.inspect(PledgeServer.create_pledge("moe", 20))
+# IO.inspect(PledgeServer.create_pledge("curly", 30))
+# IO.inspect(PledgeServer.create_pledge("daisy", 40))
+# IO.inspect(PledgeServer.create_pledge("grace", 50))
 
-IO.inspect(PledgeServer.recent_pledges(), label: "Recent pledges")
+# IO.inspect(PledgeServer.recent_pledges(), label: "Recent pledges")
