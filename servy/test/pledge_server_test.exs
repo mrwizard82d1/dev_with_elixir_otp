5052914b5f6796120bc13e31828ee70ffa75bee5
Larry Jones <mrwizard82d1@gmail.com>
2025-06-30 20:50:12 -0500

    Write unit tests Servy.PledgeServer.total/0

--- servy/test/pledge_server_test.exs
+++ servy/test/pledge_server_test.exs
@@ -20,10 +20,30 @@ defmodule PledgeServerTest do
 
     actual_last_three = PledgeServer.recent_pledges()
 
+    Process.unregister(:pledge_server)
+
     assert [
              {"grace", 50},
              {"daisy", 40},
              {"curly", 30}
            ] == actual_last_three
+  end
+
+  test "totals last three only" do
+    # Start the server
+    PledgeServer.start()
+
+    # Create pledges
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("curly", 30)
+    PledgeServer.create_pledge("daisy", 40)
+    PledgeServer.create_pledge("grace", 50)
+
+    actual_total = PledgeServer.total_pledged()
+
+    Process.unregister(:pledge_server)
+
+    assert 120 == actual_total
   end
 end
