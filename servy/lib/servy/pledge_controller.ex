cc9743f0fe4d47a4597993ed600c0725f6056e70
Larry Jones <mrwizard82d1@gmail.com>
2025-07-01 17:00:23 -0500

    Finish module 24 exercises

--- servy/lib/servy/pledge_controller.ex
+++ servy/lib/servy/pledge_controller.ex
@@ -1,15 +1,21 @@
 defmodule Servy.PledgeController do
+  alias Servy.View
+
   def create(conv, %{"name" => name, "amount" => amount}) do
     # Sends the pledge to the external service and caches it
     Servy.PledgeServer.create_pledge(name, String.to_integer(amount))
 
-    %{conv | status_code: 200, resp_body: "#{name} pledget #{amount}!"}
+    %{conv | status_code: 200, resp_body: "#{name} pledged #{amount}!"}
   end
 
   def index(conv) do
     # Get the recent pledges from the cache
     pledges = Servy.PledgeServer.recent_pledges()
 
-    %{conv | status_code: 200, resp_body: inspect(pledges)}
+    View.render(conv, "recent_pledges.eex", pledges: pledges)
+  end
+
+  def new(conv) do
+    View.render(conv, "new_pledge.eex")
   end
 end
