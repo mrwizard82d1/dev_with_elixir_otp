01c498cf195cf9ef48e870c3f731a20a6bdfd5df
Larry Jones <mrwizard82d1@gmail.com>
2025-06-30 20:42:49 -0500

    "Unit" test Server.PledgeServer.recent_pledges/0

--- servy/test/pledge_server_test.exs
+++ servy/test/pledge_server_test.exs
@@ -1,9 +1,29 @@
 defmodule PledgeServerTest do
   use ExUnit.Case
 
-  import Servy.PledgeServer
+  alias Servy.PledgeServer
 
   test "smoke test" do
     assert 2 + 2 == 4
+  end
+
+  test "caches last three only" do
+    # Start the server
+    PledgeServer.start()
+
+    # Create pledges
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("curly", 30)
+    PledgeServer.create_pledge("daisy", 40)
+    PledgeServer.create_pledge("grace", 50)
+
+    actual_last_three = PledgeServer.recent_pledges()
+
+    assert [
+             {"grace", 50},
+             {"daisy", 40},
+             {"curly", 30}
+           ] == actual_last_three
   end
 end
