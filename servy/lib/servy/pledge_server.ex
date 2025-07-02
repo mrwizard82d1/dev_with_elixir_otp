ac59d57402aa1f2e8156bc60ce28e864924966f7
Larry Jones <mrwizard82d1@gmail.com>
2025-07-02 15:33:03 -0500

    Default handling of "unhandled" message

--- servy/lib/servy/pledge_server.ex
+++ servy/lib/servy/pledge_server.ex
@@ -141,14 +141,14 @@ alias Servy.PledgeServer
 # like `{:ok, pid}`.
 {:ok, pid} = PledgeServer.start()
 
-# send(pid, {:stop, "hammertime"})
+send(pid, {:stop, "hammertime"})
 
 PledgeServer.set_cache_size(4)
 
 IO.inspect(PledgeServer.create_pledge("larry", 10))
 
 #PledgeServer.clear
-#
+
 #IO.inspect(PledgeServer.create_pledge("moe", 20))
 #IO.inspect(PledgeServer.create_pledge("curly", 30))
 #IO.inspect(PledgeServer.create_pledge("daisy", 40))
