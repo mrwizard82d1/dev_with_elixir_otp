7057cc9f1ef5b8d89f22098b9261ced144ca2305
Larry Jones <mrwizard82d1@gmail.com>
2025-07-05 19:48:32 -0500

    Link the KickStarter and HttpServer processes
    A consequence of linking these process without taking any other action
    is that when one process dies, the other process **also** dies.

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -11,6 +11,21 @@ defmodule Servy.KickStarter do
     IO.puts "Starting the HTTP server...."
 
     server_pid = spawn(Servy.HttpServer, :start, [4000])
+
+    # By linking the `HttpServer`, this process can "control" the
+    # HttpServer (Elixir) process. This linkage allows the KickStarter
+    # process to be notified if the `HttpServer` process dies and to
+    # respond appropriately.
+    #
+    # Note that the link is **bidirectional**; that is, this process
+    # is linked the `HttpServer` process **and** thee `HttpServer`
+    # process is linked to this, the `KickStarter`, process.
+    #
+    # Because the link is bidirectional and because the termination reason
+    # is "abnormal," when we kill the `HttpServer` process, the `KickStarter`
+    # process **dies as well**.
+    Process.link(server_pid)
+
     Process.register(server_pid, :http_server)
     {:ok, server_pid}
   end
