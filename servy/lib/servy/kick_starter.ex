1e7fde354755d62a95a0e961fa470e8061e7cdca
Larry Jones <mrwizard82d1@gmail.com>
2025-07-05 20:16:36 -0500

    Extract code to start HttpServer into function

--- servy/lib/servy/kick_starter.ex
+++ servy/lib/servy/kick_starter.ex
@@ -15,25 +15,8 @@ defmodule Servy.KickStarter do
     # the erlang VM.
     Process.flag(:trap_exit, true)
 
-    IO.puts "Starting the HTTP server...."
+    server_pid = start_http_server()
 
-    server_pid = spawn(Servy.HttpServer, :start, [4000])
-
-    # By linking the `HttpServer`, this process can "control" the
-    # HttpServer (Elixir) process. This linkage allows the KickStarter
-    # process to be notified if the `HttpServer` process dies and to
-    # respond appropriately.
-    #
-    # Note that the link is **bidirectional**; that is, this process
-    # is linked the `HttpServer` process **and** thee `HttpServer`
-    # process is linked to this, the `KickStarter`, process.
-    #
-    # Because the link is bidirectional and because the termination reason
-    # is "abnormal," when we kill the `HttpServer` process, the `KickStarter`
-    # process **dies as well**.
-    Process.link(server_pid)
-
-    Process.register(server_pid, :http_server)
     {:ok, server_pid}
   end
 
@@ -43,10 +26,15 @@ defmodule Servy.KickStarter do
     # the console.
     IO.puts("HttpServer exited (#{inspect reason})")
 
-    IO.puts("(Re-)Starting the HttpServer")
+    server_pid = start_http_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_http_server() do
+    IO.puts("Starting the HttpServer....")
     server_pid = spawn(Servy.HttpServer, :start, [4000])
     Process.link(server_pid)
     Process.register(server_pid, :http_server)
-    {:noreply, server_pid}
+    server_pid
   end
 end
